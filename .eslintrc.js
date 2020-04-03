e78b16ec1836e614698027a0db514b6cc31bf8ec
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-04-03 20:53:35 +0300

    feat: add eslint for check errors

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
+    semi: ["error", "never"],
+    "no-console": "off",
+    "no-use-before-define": ["error", { functions: false, variables: false }],
+    "operator-linebreak": [2, "before"],
+    "arrow-parens": [2, "as-needed"],
+    "no-restricted-syntax": [
+      "error",
+      "WithStatement",
+      "BinaryExpression[operator='in']"
+    ],
+    "no-unused-expressions": [2, { allowTernary: true }],
+    "import/extensions": [2, "ignorePackages", { js: "always" }],
+    "object-curly-newline": ["error", { multiline: true }],
+    "singleQuote": true,
+    "semi": false,
+  },
+};
