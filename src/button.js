1c7eb6c111f3d3543a9dd5a01a66f8cc3a3ef5f7
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-04-04 13:03:39 +0300

    fix: change big letters

--- src/button.js
+++ src/button.js
@@ -2,448 +2,448 @@ const arrayLetters = [
     {
         text: { en: '`', ru: 'ё' },
         width: 'small',
-        altText: { en: '~', ru: 'ё' },
+        shiftText: { en: '~', ru: 'Ё' },
         type: 'alternative',
         code: 'Backquote'
     },
     {
         text: { en: '1', ru: '1' },
         width: 'small',
-        altText: { en: '!', ru: '!' },
+        shiftText: { en: '!', ru: '!' },
         type: 'alternative',
         code: 'Digit1'
     },
     {
         text: { en: '2', ru: '2' },
         width: 'small',
-        altText: { en: '@', ru: '""' },
+        shiftText: { en: '@', ru: '""' },
         type: 'alternative',
         code: 'Digit2'
     },
     {
         text: { en: '3', ru: '3' },
         width: 'small',
-        altText: { en: '#', ru: '№' },
+        shiftText: { en: '#', ru: '№' },
         type: 'alternative',
         code: 'Digit3'
     },
     {
         text: { en: '4', ru: '4' },
         width: 'small',
-        altText: { en: '$', ru: ';' },
+        shiftText: { en: '$', ru: ';' },
         type: 'alternative',
         code: 'Digit4'
     },
     {
         text: { en: '5', ru: '5' },
         width: 'small',
-        altText: { en: '%', ru: '%' },
+        shiftText: { en: '%', ru: '%' },
         type: 'alternative',
         code: 'Digit5'
     },
     {
         text: { en: '6', ru: '6' },
         width: 'small',
-        altText: { en: '^', ru: ':' },
+        shiftText: { en: '^', ru: ':' },
         type: 'alternative',
-        code: 'Digi6'
+        code: 'Digit6'
     },
     {
         text: { en: '7', ru: '7' },
         width: 'small',
-        altText: { en: '&', ru: '?' },
+        shiftText: { en: '&', ru: '?' },
         type: 'alternative',
         code: 'Digit7'
     },
     {
         text: { en: '8', ru: '8' },
         width: 'small',
-        altText: { en: '*', ru: '*' },
+        shiftText: { en: '*', ru: '*' },
         type: 'alternative',
         code: 'Digit8'
     },
     {
         text: { en: '9', ru: '9' },
         width: 'small',
-        altText: { en: '(', ru: '(' },
+        shiftText: { en: '(', ru: '(' },
         type: 'alternative',
         code: 'Digit9'
     },
     {
         text: { en: '0', ru: '0' },
         width: 'small',
-        altText: { en: ')', ru: ')' },
+        shiftText: { en: ')', ru: ')' },
         type: 'alternative',
         code: 'Digit0'
     },
     {
         text: { en: '-', ru: '-' },
         width: 'small',
-        altText: { en: '_', ru: '_' },
+        shiftText: { en: '_', ru: '_' },
         type: 'alternative',
         code: 'Minus'
     },
     {
         text: { en: '=', ru: '=' },
         width: 'small',
-        altText: { en: '+', ru: '+' },
+        shiftText: { en: '+', ru: '+' },
         type: 'alternative',
         code: 'Equal'
     },
     {
         text: { en: 'Backspace', ru: 'Backspace' },
         width: 'large',
-        altText: { en: 'Backspace', ru: 'Backspace' },
+        shiftText: { en: 'Backspace', ru: 'Backspace' },
         type: 'functional',
         code: 'Backspace'
     },
     {
         text: { en: 'Tab', ru: 'Tab' },
         width: 'medium',
-        altText: { en: 'Tab', ru: 'Tab' },
+        shiftText: { en: 'Tab', ru: 'Tab' },
         type: 'functional',
         code: 'Tab'
     },
     {
         text: { en: 'q', ru: 'й' },
         width: 'small',
-        altText: { en: 'q', ru: 'й' },
+        shiftText: { en: 'q', ru: 'Й' },
         type: 'word',
         code: 'KeyQ'
     },
     {
         text: { en: 'w', ru: 'ц' },
         width: 'small',
-        altText: { en: 'w', ru: 'ц' },
+        shiftText: { en: 'w', ru: 'Ц' },
         type: 'word',
         code: 'KeyW'
     },
     {
         text: { en: 'e', ru: 'у' },
         width: 'small',
-        altText: { en: 'e', ru: 'у' },
+        shiftText: { en: 'e', ru: 'У' },
         type: 'word',
         code: 'KeyE'
     },
     {
         text: { en: 'r', ru: 'к' },
         width: 'small',
-        altText: { en: 'r', ru: 'к' },
+        shiftText: { en: 'r', ru: 'К' },
         type: 'word',
         code: 'KeyR'
     },
     {
         text: { en: 't', ru: 'е' },
         width: 'small',
-        altText: { en: 't', ru: 'е' },
+        shiftText: { en: 't', ru: 'Е' },
         type: 'word',
         code: 'KeyT'
     },
     {
         text: { en: 'y', ru: 'н' },
         width: 'small',
-        altText: { en: 'y', ru: 'н' },
+        shiftText: { en: 'y', ru: 'Н' },
         type: 'word',
         code: 'KeyY'
     },
     {
         text: { en: 'u', ru: 'г' },
         width: 'small',
-        altText: { en: 'u', ru: 'г' },
+        shiftText: { en: 'u', ru: 'Г' },
         type: 'word',
         code: 'KeyU'
     },
     {
         text: { en: 'i', ru: 'ш' },
         width: 'small',
-        altText: { en: 'i', ru: 'ш' },
+        shiftText: { en: 'i', ru: 'Ш' },
         type: 'word',
         code: 'KeyI'
     },
     {
         text: { en: 'o', ru: 'щ' },
         width: 'small',
-        altText: { en: 'o', ru: 'щ' },
+        shiftText: { en: 'o', ru: 'Щ' },
         type: 'word',
         code: 'KeyO'
     },
     {
         text: { en: 'p', ru: 'з' },
         width: 'small',
-        altText: { en: 'p', ru: 'з' },
+        shiftText: { en: 'p', ru: 'З' },
         type: 'word',
         code: 'KeyP'
     },
     {
         text: { en: '[', ru: 'х' },
         width: 'small',
-        altText: { en: '{', ru: 'х' },
+        shiftText: { en: '{', ru: 'Х' },
         type: 'alternative',
         code: 'BracketLeft'
     },
     {
         text: { en: ']', ru: 'ъ' },
         width: 'small',
-        altText: { en: '}', ru: 'ъ' },
+        shiftText: { en: '}', ru: 'Ъ' },
         type: 'alternative',
         code: 'BracketRight'
     },
     {
         text: { en: '\\', ru: '\\' },
         width: 'small',
-        altText: { en: '|', ru: '/' },
+        shiftText: { en: '|', ru: '/' },
         type: 'alternative',
         code: 'Backslash'
     },
     {
         text: { en: 'DEL', ru: 'DEL' },
         width: 'medium',
-        altText: { en: 'DEL', ru: 'DEL' },
+        shiftText: { en: 'DEL', ru: 'DEL' },
         type: 'functional',
         code: 'Delete'
     },
     {
         text: { en: 'CapsLock', ru: 'CapsLock' },
         width: 'large',
-        altText: { en: 'CapsLock', ru: 'CapsLock' },
+        shiftText: { en: 'CapsLock', ru: 'CapsLock' },
         type: 'functional',
         code: 'CapsLock'
     },
     {
         text: { en: 'a', ru: 'ф' },
         width: 'small',
-        altText: { en: 'a', ru: 'ф' },
+        shiftText: { en: 'a', ru: 'Ф' },
         type: 'word',
         code: 'KeyA'
     },
     {
         text: { en: 's', ru: 'ы' },
         width: 'small',
-        altText: { en: 's', ru: 'ы' },
+        shiftText: { en: 's', ru: 'Ы' },
         type: 'word',
         code: 'KeyS'
     },
     {
         text: { en: 'd', ru: 'в' },
         width: 'small',
-        altText: { en: 'd', ru: 'в' },
+        shiftText: { en: 'd', ru: 'В' },
         type: 'word',
         code: 'KeyD'
     },
     {
         text: { en: 'f', ru: 'а' },
         width: 'small',
-        altText: { en: 'f', ru: 'а' },
+        shiftText: { en: 'f', ru: 'А' },
         type: 'word',
         code: 'KeyF'
     },
     {
         text: { en: 'g', ru: 'п' },
         width: 'small',
-        altText: { en: 'g', ru: 'п' },
+        shiftText: { en: 'g', ru: 'П' },
         type: 'word',
         code: 'KeyG'
     },
     {
         text: { en: 'h', ru: 'р' },
         width: 'small',
-        altText: { en: 'h', ru: 'р' },
+        shiftText: { en: 'h', ru: 'Р' },
         type: 'word',
         code: 'KeyH'
     },
     {
         text: { en: 'j', ru: 'о' },
         width: 'small',
-        altText: { en: 'j', ru: 'о' },
+        shiftText: { en: 'j', ru: 'О' },
         type: 'word',
         code: 'KeyJ'
     },
     {
         text: { en: 'k', ru: 'л' },
         width: 'small',
-        altText: { en: 'l', ru: 'л' },
+        shiftText: { en: 'l', ru: 'Л' },
         type: 'word',
         code: 'KeyK'
     },
     {
         text: { en: 'l', ru: 'д' },
         width: 'small',
-        altText: { en: 'l', ru: 'д' },
+        shiftText: { en: 'l', ru: 'Д' },
         type: 'word',
         code: 'KeyL'
     },
     {
         text: { en: ';', ru: 'ж' },
         width: 'small',
-        altText: { en: ':', ru: 'ж' },
+        shiftText: { en: ':', ru: 'Ж' },
         type: 'alternative',
         code: 'Semicolon'
     },
     {
         text: { en: "''", ru: 'э' },
         width: 'small',
-        altText: { en: "''", ru: 'э' },
+        shiftText: { en: "''", ru: 'Э' },
         type: 'alternative',
         code: 'Quote'
     },
     {
         text: { en: 'ENTER', ru: 'ENTER' },
         width: 'large',
-        altText: { en: 'ENTER', ru: 'ENTER' },
+        shiftText: { en: 'ENTER', ru: 'ENTER' },
         type: 'functional',
         code: 'Enter',
     },
     {
         text: { en: 'Shift', ru: 'Shift' },
         width: 'large',
-        altText: { en: 'Shift', ru: 'Shift' },
+        shiftText: { en: 'Shift', ru: 'Shift' },
         type: 'functional',
         code: 'ShiftLeft',
     },
     {
         text: { en: 'z', ru: 'я' },
         width: 'small',
-        altText: { en: 'z', ru: 'я' },
+        shiftText: { en: 'z', ru: 'Я' },
         type: 'word',
         code: 'KeyZ'
     },
     {
         text: { en: 'x', ru: 'ч' },
         width: 'small',
-        altText: { en: 'x', ru: 'ч' },
+        shiftText: { en: 'x', ru: 'Ч' },
         type: 'word',
         code: 'KeyX'
     },
     {
         text: { en: 'c', ru: 'с' },
         width: 'small',
-        altText: { en: 'c', ru: 'с' },
+        shiftText: { en: 'c', ru: 'С' },
         type: 'word',
         code: 'KeyC'
     },
     {
         text: { en: 'v', ru: 'м' },
         width: 'small',
-        altText: { en: 'v', ru: 'м' },
+        shiftText: { en: 'v', ru: 'М' },
         type: 'word',
         code: 'KeyV'
     },
     {
         text: { en: 'b', ru: 'и' },
         width: 'small',
-        altText: { en: 'b', ru: 'и' },
+        shiftText: { en: 'b', ru: 'И' },
         type: 'word',
         code: 'KeyB'
     },
     {
         text: { en: 'n', ru: 'т' },
         width: 'small',
-        altText: { en: 'n', ru: 'т' },
+        shiftText: { en: 'n', ru: 'Т' },
         type: 'word',
         code: 'KeyN'
     },
     {
         text: { en: 'm', ru: 'ь' },
         width: 'small',
-        altText: { en: 'm', ru: 'ь' },
+        shiftText: { en: 'm', ru: 'Ь' },
         type: 'word',
         code: 'KeyM'
     },
     {
         text: { en: ',', ru: 'б' },
         width: 'small',
-        altText: { en: '<', ru: 'б' },
+        shiftText: { en: '<', ru: 'Б' },
         type: 'alternative',
         code: 'Comma'
     },
     {
         text: { en: '.', ru: 'ю' },
         width: 'small',
-        altText: { en: '>', ru: 'ю' },
+        shiftText: { en: '>', ru: 'Ю' },
         type: 'alternative',
         code: 'Period'
     },
     {
         text: { en: '/', ru: '.' },
         width: 'small',
-        altText: { en: '?', ru: ',' },
+        shiftText: { en: '?', ru: ',' },
         type: 'alternative',
         code: 'Slash'
     },
     {
         text: { en: '&#9650;', ru: '&#9650;' },
         width: 'small',
-        altText: { en: '&#9650;', ru: '&#9650;' },
+        shiftText: { en: '&#9650;', ru: '&#9650;' },
         type: 'functional',
         code: 'ArrowUp',
     },
     {
         text: { en: 'Shift', ru: 'Shift' },
         width: 'large',
-        altText: { en: 'Shift', ru: 'Shift' },
+        shiftText: { en: 'Shift', ru: 'Shift' },
         type: 'functional',
         code: 'ShiftRight',
     },
     {
         text: { en: 'Ctrl', ru: 'Ctrl' },
         width: 'medium',
-        altText: { en: 'Ctrl', ru: 'Ctrl' },
+        shiftText: { en: 'Ctrl', ru: 'Ctrl' },
         type: 'functional',
         code: 'ControlLeft',
     },
     {
         text: { en: 'Win', ru: 'Win' },
         width: 'medium',
-        altText: { en: 'Win', ru: 'Win' },
+        shiftText: { en: 'Win', ru: 'Win' },
         type: 'functional',
         code: 'OSLeft',
     },
     {
         text: { en: 'Alt', ru: 'Alt' },
         width: 'medium',
-        altText: { en: 'Alt', ru: 'Alt' },
+        shiftText: { en: 'Alt', ru: 'Alt' },
         type: 'functional',
         code: 'AltLeft',
     },
     {
         text: { en: ' ', ru: ' ' },
         width: 'x-large',
-        altText: { en: ' ', ru: ' ' },
+        shiftText: { en: ' ', ru: ' ' },
         type: 'word',
         code: 'Space',
     },
     {
         text: { en: 'Alt', ru: 'Alt' },
         width: 'medium',
-        altText: { en: 'Alt', ru: 'Alt' },
+        shiftText: { en: 'Alt', ru: 'Alt' },
         type: 'functional',
         code: 'AltRight',
     },
     {
         text: { en: '&#9668;', ru: '&#9668;' },
         width: 'small',
-        altText: { en: '&#9668;', ru: '&#9668;' },
+        shiftText: { en: '&#9668;', ru: '&#9668;' },
         type: 'functional',
         code: 'ArrowLeft',
     },
     {
         text: { en: '&#9660;', ru: '&#9660;' },
         width: 'small',
-        altText: { en: '&#9660;', ru: '&#9660;' },
+        shiftText: { en: '&#9660;', ru: '&#9660;' },
         type: 'functional',
         code: 'ArrowDown',
     },
     {
         text: { en: '&#9658;', ru: '&#9658;' },
         width: 'small',
-        altText: { en: '&#9658;', ru: '&#9658;' },
+        shiftText: { en: '&#9658;', ru: '&#9658;' },
         type: 'functional',
         code: 'ArrowRight',
     },
     {
         text: { en: 'Ctrl', ru: 'Ctrl' },
         width: 'medium',
-        altText: { en: 'Ctrl', ru: 'Ctrl' },
+        shiftText: { en: 'Ctrl', ru: 'Ctrl' },
         type: 'functional',
         code: 'ControlRight',
     },
